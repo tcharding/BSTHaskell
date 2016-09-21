7d99a46a1ec733042af3d0b63095801cc23adf4c
tcharding <me@tobin.cc>
2016-09-21 13:41:30 +1000

    added foldT, updated Tree data type

--- BST.hs
+++ BST.hs
@@ -1,90 +1,73 @@
-import Data.List
+{-# OPTIONS_GHC -Wall #-}
 
 {-
  Binary Search Tree 
 -}
 
-data Tree a = Node a (Tree a) (Tree a)
+data Tree a = Node (Tree a) a (Tree a)
             | Nil
               deriving (Eq)
 
 instance Show a => Show (Tree a)
   where show Nil = "Nil"
-        show (Node x Nil Nil) = show x
-        show (Node x l r) = "([" ++ show x ++ "] " ++ show l ++ " " ++ show r ++ ")"
+        show (Node Nil x Nil) = show x
+        show (Node l x r) = "([" ++ show x ++ "] " ++ show l ++ " " ++ show r ++ ")"
 
-height :: Tree a -> Int
-height Nil = 0
-height (Node _ l r) = 1 + max (height l) (height r)
+-- ref: http://www.seas.upenn.edu/~cis194/spring13/lectures/07-folds-monoids.html
+foldT :: b -> (b -> a -> b -> b) -> Tree a -> b
+foldT e _ Nil = e
+foldT e f (Node l x r) = f (foldT e f l) x (foldT e f r)
 
-size :: Tree a -> Int
-size Nil = 0
-size (Node _ l r) = 1 + (size l) + (size r)
+size :: Tree a -> Integer
+size = foldT 0 (\l _ r -> 1 + l + r)
 
-minT :: Ord a => Tree a -> a
-minT (Node v Nil Nil) = v
-minT (Node v Nil _) = v
-minT (Node v l r) = minT l
+sumT :: Tree Integer -> Integer
+sumT = foldT 0 (\l x r -> l + x + r)
 
-maxT :: Tree a -> a
-maxT (Node v Nil Nil) = v
-maxT (Node v _ Nil) = v
-maxT (Node v l r) = maxT r
+height :: Tree a -> Integer
+height = foldT 0 (\l _ r -> 1 + max l r)
 
-preOrder :: Tree a -> [a]
-preOrder (Node v l r) = [v] ++ inOrder l ++ inOrder r
-preOrder _ = []
+flatten :: Tree a -> [a]
+flatten = foldT [] (\l x r -> l ++ [x] ++ r)
 
-inOrder :: Tree a -> [a]
-inOrder (Node v l r) = inOrder l ++ [v] ++ inOrder r
-inOrder _ = []
+maxT :: (Ord a, Bounded a) => Tree a -> a
+maxT = foldT minBound (\l x r -> l `max` x `max` r)
 
-postOrder :: Tree a -> [a]
-postOrder (Node v l r) = inOrder l ++ inOrder r ++ [v]
-postOrder _ = []
+minT :: (Ord a, Bounded a) => Tree a -> a
+minT = foldT maxBound (\l x r -> l `min` x `min` r)
 
-search :: (Ord a,Eq a) => a -> Tree a -> Bool
-search x Nil = False
-search x (Node v l r)
-  | x == v = True
-  | x < v = search x l
-  | otherwise = search x r
-
-{-
-can we get these in a funcitonal implementation?
-  i.e with out a parent pointer
-predecessor :: Tree a -> Maybe a
-predecessor (Node _ (Node v _ _) _) = Just v 
-predecessor _ = undefined
-
-successor :: Ord a => Tree a -> Maybe a
-successor (Node _ l r) = Just (minT r)
-successor _ = undefined
--}
+elemT :: Eq a => a -> Tree a -> Bool
+elemT x (Node l y r) = x == y || (elemT x l) || (elemT x r)
+elemT _ Nil = False
 
 -- supports multiple nodes of same value
 add :: Ord a => Tree a -> a -> Tree a
-add Nil x = (Node x Nil Nil)
-add (Node v l r) x
-  | x < v = Node v (add l x) r
-  | otherwise = Node v l (add r x)
+add Nil x = (Node Nil x Nil)
+add (Node l v r) x
+  | x < v = Node (add l x) v r
+  | otherwise = Node l v (add r x)
 
 -- ref: Introduction to Algorithms
 --  Cormen, Leiserson, Rivest, Stein
-deleteT :: (Eq a,Ord a) => a -> Tree a -> Tree a
---deleteT x Nil = Nil
---deleteT x (Node v Nil Nil) = Nil
-deleteT x (Node v Nil r)
+deleteT :: Ord a => a -> Tree a -> Tree a
+
+deleteT x (Node Nil v r)
   | x == v = r
-deleteT x (Node v l Nil)
+
+deleteT x (Node l v Nil)
   | x == v = l
-deleteT x (Node v l (Node w Nil r))
-  | x == v = Node w l r
-deleteT x (Node v l (Node a (Node b Nil r) s))
-  | x == v = Node b l (Node a r s)
-deleteT x (Node v l r)
-  | x < v = Node v (deleteT x l) r
-  | otherwise = Node v l (deleteT x r)
+
+deleteT x (Node l v (Node Nil w r))
+  | x == v = Node l w r
+
+deleteT x (Node l v (Node (Node Nil b r) a s))
+  | x == v = Node l b (Node r a s)
+
+deleteT x (Node l v r)
+  | x < v = Node (deleteT x l) v r
+  | otherwise = Node l v (deleteT x r)
+
+deleteT _ Nil = Nil
 
 -- naive implementation
 buildT :: Ord a => [a] -> Tree a
@@ -92,46 +75,3 @@ buildT = naive Nil
   where naive t (x:xs) = naive (add t x) xs
         naive t [] = t
 
-{-
- Rudementry testing by someone who hasn't yet learnt monads or Haskell test frameworks
--}
-
-t = buildT [3,5,2,4,1]
-
-{-
-tests = [testMinT]
-
-test [] = "ok"
-test (t:ts) = 
-  if snd (t) == False
-    then "Test failed: " ++ fst t
-    else test ts
-
-testMinT = ("MinT", minT t == 1)
--}
-
-test = do
-  testMinT
-  testMaxT
-
-testMinT =
-  if (minT t) /= 1
-    then tf "minT"
-    else ""   
-
-testMaxT =
-  if (maxT t) /= 5
-    then tf "maxT"
-    else ""   
-
-testHeight =
-  if (height t) /= 3
-  then tf "height"
-  else ""
-
-testSize =
-  if (size t) /= 3
-  then tf "size"
-  else ""
-  
-tf msg = error ("test failed: " ++ msg)
